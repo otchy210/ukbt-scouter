1b26183f219bf7b0f4de3832d93fb9fcc564f0e5
Otchy <id+github@otchy.net>
2021-05-03 23:36:04 +0900

    Add sample code for database and storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
-const App = () => {
-    return <div>app</div>;
+const App = (props) => {
+    const {db, storage} = props;
+    const fileRef = useRef();
+    return <div>
+        <button onClick={() => {
+            const now = Date.now();
+            const ref = db.ref('now');
+            ref.set({now}, (error) => {
+                if (error) {
+                    console.error('something wrong', error);
+                } else {
+                    console.log('now is updated');
+                }
+            });
+        }}>set now</button>
+        <label style={{display: 'block', cursor: 'pointer'}}>
+            Upload
+            <input type="file" ref={fileRef} style={{display: 'none'}} onChange={() => {
+                const fileInput = fileRef.current;
+                if (fileInput.files.length === 0) {
+                    return;
+                }
+                const file = fileInput.files[0];
+                const fileName = file.name;
+                storage.ref(fileName).put(file).then((result) => {
+                    console.log('file uploaded', result);
+                });
+            }} />
+        </label>
+    </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,25 +3,12 @@ import { render } from 'react-dom';
 import App from './components/App.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // // The Firebase SDK is initialized and available here!
-    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
     try {
         const app = firebase.app();
-        const features = [
-            'auth', 
-            'database', 
-            'firestore',
-            'functions',
-            'messaging', 
-            'storage', 
-            'analytics', 
-            'remoteConfig',
-            'performance',
-        ].filter(feature => typeof app[feature] === 'function');
-        console.log(`Firebase SDK loaded with ${features.join(', ')}`);
+        const db = app.database();
+        const storage = app.storage();
+        render(<App db={db} storage={storage}></App>, document.getElementById('app'));
     } catch (e) {
         console.error(e);
     }
-    render(<App></App>, document.getElementById('app'));
 });
