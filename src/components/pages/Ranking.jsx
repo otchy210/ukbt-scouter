a8198197fdc4e0673fb596f939943befb64aa7a9
Otchy <id+github@otchy.net>
2021-05-20 00:04:39 +0900

    Implement ranking page

--- src/components/pages/Ranking.jsx
+++ src/components/pages/Ranking.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../blocks/Loading.jsx';
 import Records from '../blocks/Records.jsx';
+import { useRanking } from '../libs/Database.js';
 
 const Ranking = () => {
-    return <>
+    const [ranking, setRanking] = useState();
+
+    useEffect(async () => {
+        const ranking = await useRanking();
+        setRanking(ranking);
+    }, []);
+
+    if (!ranking) {
+        return <Loading />;
+    }
+
+return <>
         <h1>ランキング</h1>
-        <Records />
+        <Records records={ranking} pagination={true} />
     </>
 };
 
